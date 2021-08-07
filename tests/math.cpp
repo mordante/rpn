6558d387f7b4630288db6cb3a3d8c426765ee1d1
Mark de Wever <koraq@xs4all.nl>
2021-08-07 16:43:08 +0200

    Implement the basic arithmetic operations.
    The add has already been implemented, now implement
    - subtract
    - multiply
    - divide, for now always an integral operation, truncating the value
    
    Fixes a bug were the calculation was done in the wrong order. This doesn't
    affect addition, but it becomes obvious with subtraction.
    
    Improves the valiation when the stack contains too few elements to do the
    calculation. Before a one element stack for a binary operation resulted in an
    empty stack.

--- tests/math.cpp
+++ tests/math.cpp
@@ -22,3 +22,29 @@ TEST(math, add) {
   EXPECT_EQ(math::add(1, 0), 1);
   EXPECT_EQ(math::add(1, 1), 2);
 }
+
+TEST(math, sub) {
+  EXPECT_EQ(math::sub(0, 0), 0);
+  EXPECT_EQ(math::sub(0, 1), -1);
+  EXPECT_EQ(math::sub(1, 0), 1);
+  EXPECT_EQ(math::sub(1, 1), 0);
+}
+
+TEST(math, mul) {
+  EXPECT_EQ(math::mul(0, 0), 0);
+  EXPECT_EQ(math::mul(2, -2), -4);
+  EXPECT_EQ(math::mul(-2, 2), -4);
+  EXPECT_EQ(math::mul(-2, -2), 4);
+  EXPECT_EQ(math::mul(2, 2), 4);
+}
+
+TEST(math, div) {
+  EXPECT_EQ(math::div(0, 1), 0);
+  EXPECT_EQ(math::div(2, -2), -1);
+  EXPECT_EQ(math::div(-2, 2), -1);
+  EXPECT_EQ(math::div(-2, -2), 1);
+  EXPECT_EQ(math::div(2, 2), 1);
+  EXPECT_EQ(math::div(4, 2), 2);
+  EXPECT_EQ(math::div(4, 3), 1);
+  EXPECT_EQ(math::div(3, 4), 0);
+}
