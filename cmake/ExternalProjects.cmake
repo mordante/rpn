90b0f93383d8d70df474b8b8f3ace2fb848a93cb
Mark de Wever <koraq@xs4all.nl>
2021-08-07 10:04:37 +0200

    Add an initial GUI.
    Just an intial experiment to have some kind of GUI for the application.

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -1,6 +1,18 @@
 include(FetchContent)
 
 FetchContent_Declare(
+  fltk
+  GIT_REPOSITORY https://github.com/fltk/fltk
+  GIT_TAG        release-1.3.7
+)
+FetchContent_GetProperties(fltk)
+if(NOT fltk_POPULATED)
+  FetchContent_Populate(fltk)
+  set(FLTK_BUILD_TEST OFF CACHE BOOL "" FORCE)
+  add_subdirectory(${fltk_SOURCE_DIR} ${fltk_BINARY_DIR})
+endif()
+
+FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG        release-1.11.0
