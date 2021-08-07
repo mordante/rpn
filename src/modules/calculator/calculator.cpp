49990dde5c2898409c7e1675aae746ca2a8ff09a
Mark de Wever <koraq@xs4all.nl>
2021-08-07 10:04:37 +0200

    Toy with a GUI.
    Just an intial experiment to have some kind of GUI for the application.

---
+++ src/modules/calculator/calculator.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) Mark de Wever <koraq@xs4all.nl>
+ * Part of the RPN project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ *
+ * See the COPYING file for more details.
+ */
+
+export module calculator;
+
+export import calculator.controller;
+export import calculator.model;
+export import calculator.value;
