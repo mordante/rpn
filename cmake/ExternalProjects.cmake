0dbc61afb26847f1de401b0e734716dd4f1cdb9a
Mark de Wever <koraq@xs4all.nl>
2021-08-07 10:04:37 +0200

    Toy with a GUI.
    Just an intial experiment to have some kind of GUI for the application.

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -1,5 +1,30 @@
 include(FetchContent)
 
+#FetchContent_Declare(
+#  imgui
+#  GIT_REPOSITORY https://github.com/ocornut/imgui
+#  GIT_TAG v1.83
+#)
+#FetchContent_GetProperties(imgui)
+#if(NOT imgui_POPULATED)
+#  FetchContent_Populate(imgui)
+##  add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR})
+#endif()
+
+FetchContent_Declare(
+  fltk
+  GIT_REPOSITORY https://github.com/fltk/fltk
+  GIT_TAG        release-1.3.7
+)
+FetchContent_GetProperties(fltk)
+if(NOT fltk_POPULATED)
+  FetchContent_Populate(fltk)
+  set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+  add_subdirectory(${fltk_SOURCE_DIR} ${fltk_BINARY_DIR})
+endif()
+
+
+
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
