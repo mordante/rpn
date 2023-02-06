6aa0e2a2a2b4501b6228853d29234b88890a17f1
Mark de Wever <koraq@xs4all.nl>
2023-02-06 17:36:47 +0100

    Undos cmake formatting.
    The formatting looks quite bad, so revert these changes.

--- cmake/ClangModule.cmake
+++ cmake/ClangModule.cmake
@@ -1,16 +1,23 @@
 # Adds a module to the system.
 #
 # This code has originally been copied from
-# https://stackoverflow.com/a/62499857. The usage of the library modules in the
-# system is heavily inspired by the same post and the clang modules
-# documentation.
+# https://stackoverflow.com/a/62499857.
+# The usage of the library modules in the system is heavily inspired by the
+# same post and the clang modules documentation.
 function(add_module name)
-  file(MAKE_DIRECTORY ${PREBUILT_MODULE_PATH})
-  add_custom_target(
-    ${name}.pcm
-    COMMAND
-      ${CMAKE_CXX_COMPILER} -std=c++2b -stdlib=libc++ -fexperimental-library
-      -fmodules -fprebuilt-module-path=${PREBUILT_MODULE_PATH} -c
-      ${CMAKE_CURRENT_SOURCE_DIR}/${ARGN} -Xclang -emit-module-interface -o
-      ${PREBUILT_MODULE_PATH}/${name}.pcm)
+    file(MAKE_DIRECTORY ${PREBUILT_MODULE_PATH})
+    add_custom_target(${name}.pcm
+            COMMAND
+                ${CMAKE_CXX_COMPILER}
+                -std=c++2b
+                -stdlib=libc++
+				-fexperimental-library
+                -fmodules
+				-fprebuilt-module-path=${PREBUILT_MODULE_PATH}
+                -c
+                ${CMAKE_CURRENT_SOURCE_DIR}/${ARGN}
+                -Xclang -emit-module-interface
+                -o ${PREBUILT_MODULE_PATH}/${name}.pcm
+
+            )
 endfunction()
