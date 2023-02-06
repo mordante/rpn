6e242a8c271183d460183509a4c9c80a2f2cdcc2
Mark de Wever <koraq@xs4all.nl>
2023-02-06 17:36:47 +0100

    Undo cmake formatting

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -3,28 +3,25 @@ include(FetchContent)
 FetchContent_Declare(
   fltk
   GIT_REPOSITORY https://github.com/fltk/fltk
-  GIT_TAG release-1.3.8)
+  GIT_TAG        release-1.3.8
+)
 FetchContent_GetProperties(fltk)
 if(NOT fltk_POPULATED)
   FetchContent_Populate(fltk)
-  set(FLTK_BUILD_TEST
-      OFF
-      CACHE BOOL "" FORCE)
+  set(FLTK_BUILD_TEST OFF CACHE BOOL "" FORCE)
   add_subdirectory(${fltk_SOURCE_DIR} ${fltk_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()
 
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG release-1.11.0)
+  GIT_TAG        release-1.11.0
+)
 FetchContent_GetProperties(googletest)
 if(NOT googletest_POPULATED)
   FetchContent_Populate(googletest)
-  set(BUILD_GMOCK
-      OFF
-      CACHE BOOL "" FORCE)
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR}
-                   EXCLUDE_FROM_ALL)
+  set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()
 
 FetchContent_Declare(
@@ -32,7 +29,8 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
   # The 3.0.0 release doesn't build with libc++16 due to usage of deprecated
   # constructs.
-  GIT_TAG 60b9e49)
+  GIT_TAG 60b9e49
+)
 FetchContent_GetProperties(ftxui)
 if(NOT ftxui_POPULATED)
   FetchContent_Populate(ftxui)
