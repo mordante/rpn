b314c6184e978f0be810601242c80ad690684bf1
Mark de Wever <koraq@xs4all.nl>
2023-02-06 17:36:49 +0100

    Implements log operations.
    This adds log operations for the following bases, 2, e, 10.

---
+++ src/modules/calculator/math/logarithm.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) Mark de Wever <koraq@xs4all.nl>
+ * Part of the RPN project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ *
+ * See the COPYING file for more details.
+ */
+
+export module calculator.math.logarithm;
+
+export import calculator.math.core;
+
+export import <variant>;
+import <cmath>;
+
+namespace calculator {
+namespace math {
+
+// Note since logarithms are expected to be used on floating-point values these
+// functions always return a floating-point value.
+export tstorage lg(tstorage value) { return std::log2(double_cast(value)); }
+
+export tstorage ln(tstorage value) { return std::log(double_cast(value)); }
+
+export tstorage log(tstorage value) { return std::log10(double_cast(value)); }
+
+} // namespace math
+} // namespace calculator
